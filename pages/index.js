1e2d35a111ced71d72927a6917a0367515efef3f
TankyTurkey <91321021+TankyTurkey@users.noreply.github.com>
2021-09-25 09:55:25 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -51,35 +51,35 @@ A Total of 366+1 art pieces on OpenSea NFTs Marketplace, 366 published on polygo
 1 outstanding piece to be published on ethereum network soon.
 </div>
         <div className={styles.grid}>
-          <a href="https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/81210281507696217995685449912678685468372656662771121513866832894761057124353/" className={styles.card}>
-            <h2>Tanky Turkey #01</h2>
+          <a href="https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/27297555424367449018882142262532668847556185165117409212162522491783292649473/" className={styles.card}>
+            <h2>Tanky Turkey #001</h2>
             <img 
-              src="https://res.cloudinary.com/luneswallet/image/upload/c_scale,w_102/v1630285167/pixel-saga-nft/01.png"
-              alt="Tanky Turkey #01"
+              src="https://lh3.googleusercontent.com/cJBpZJbhrifDGSdERlYmgkGb1rXCB53-S0dH-lGPrPoQq0_NNr2VloAfI3DsqC8qBNxWgSMGQ1eM3NZnr6xOVZCzwQmWUtITXRCL=w305"
+              alt="Tanky Turkey #001"
             />
           </a>
 
-          <a href="https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/81210281507696217995685449912678685468372656662771121513866832895860568752129/" className={styles.card}>
-            <h2>Tanky Turkey #02</h2>
+          <a href="https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/27297555424367449018882142262532668847556185165117409212162522499479874043905/" className={styles.card}>
+            <h2>Tanky Turkey #008</h2>
             <img 
-              src="https://res.cloudinary.com/luneswallet/image/upload/c_scale,w_102/v1630285162/pixel-saga-nft/02.png"
-              alt="Tanky Turkey #02"
+              src="https://lh3.googleusercontent.com/Xp_KYT05n1DoodSFqDvKjtNm5lvv8PCuYt1G7Q2A5Euc1gNhsN6FRgSar2SHo7plsHRmm9O8e789jnEQP3ypaQG8iF4GTJJ7g62P=w305"
+              alt="Tanky Turkey #008"
             />
           </a>
 
           <a href="https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/81210281507696217995685449912678685468372656662771121513866832896960080379905/" className={styles.card}>
-            <h2>Tanky Turkey #03</h2>
+            <h2>Tanky Turkey #013</h2>
             <img 
-              src="https://res.cloudinary.com/luneswallet/image/upload/c_scale,w_102/v1630285182/pixel-saga-nft/pixil-frame-0_1.png"
-              alt="Tanky Turkey #03"
+              src="https://lh3.googleusercontent.com/pk9fShV1E8JsK6dcAhmmrSwpxEcRihDS0gPcOrjnQqj4IEqXoGJTf12ALHN0pbH83ibyY4AH0gyPS24E9lNPnG_rYIkRj6gXr1jQ=w305"
+              alt="Tanky Turkey #013"
             />
           </a>
 
-          <a href="https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/81210281507696217995685449912678685468372656662771121513866832898059592007681/" className={styles.card}>
-            <h2>Tanky Turkey #04</h2>
+          <a href="https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/27297555424367449018882142262532668847556185165117409212162522520370594971649/" className={styles.card}>
+            <h2>Tanky Turkey #027</h2>
             <img 
-              src="https://res.cloudinary.com/luneswallet/image/upload/c_scale,w_102/v1630288806/pixel-saga-nft/04.png"
-              alt="Tanky Turkey #04"
+              src="https://lh3.googleusercontent.com/8QhCH21MkXAK71x5dpCBUSsikY0EaDSw67lTe6vV6wJ789nTDEfXr_rmWRyDV3j0tldJyT4j36-WAVXR5tMC6QHXD3F5sLKCnB2szg=w305"
+              alt="Tanky Turkey #027"
             />
           </a>
         </div>
